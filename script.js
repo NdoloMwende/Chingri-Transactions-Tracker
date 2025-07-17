21df6c91f70a612867b0c54c2c5cedbc2ee49d3d
Ndolo Mercy <mercymwendend444@gmail.com>
2025-07-17 22:59:34 +0300

    Updated elements in script.js

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const fetchBtn = document.getElementById("fetchDataBtn");
   const tableBody = document.querySelector("#transaction-table tbody");
   const totalDisplay = document.getElementById("totalDisplay");
+   const alertBox = document.getElementById("alertBox");
+  const adviceBox = document.getElementById("adviceBox");
 });
