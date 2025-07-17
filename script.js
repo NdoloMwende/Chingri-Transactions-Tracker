db610b431a7a85e8514865e6692c0e732c7be236
Ndolo Mercy <mercymwendend444@gmail.com>
2025-07-17 14:08:55 +0300

    Grabbed DOM elements

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  /* ===== ELEMENTS ===== */
+  const fetchBtn = document.getElementById("fetchDataBtn");
+  const tableBody = document.querySelector("#transaction-table tbody");
+  const totalDisplay = document.getElementById("totalDisplay");
+});
