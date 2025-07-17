2126fde3fdb3b796bc209176f9c72def813f9c9f
Ndolo Mercy <mercymwendend444@gmail.com>
2025-07-17 23:21:00 +0300

    Added alert frequency section

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  /* ===== ELEMENTS ===== */
+// ELEMENTS 
   const fetchBtn = document.getElementById("fetchDataBtn");
   const tableBody = document.querySelector("#transaction-table tbody");
   const totalDisplay = document.getElementById("totalDisplay");
    const alertBox = document.getElementById("alertBox");
   const adviceBox = document.getElementById("adviceBox");
+
+  //Dropdown Items
+   
 });
